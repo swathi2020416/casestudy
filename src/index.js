d4dfc0952150b3018edaffad12193a980aa9a504
swathi2020416 <prswathi.mpl@gmail.com>
2020-10-12 08:18:30 +0530

    casestudysample

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './components/store/reducers/rootReducer'
+const store = createStore(rootReducer);
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+serviceWorker.unregister();
